d722736322fd77bc953173533e0caa2e08e42626
yh <yhl124.py@gmail.com>
2023-12-29 01:54:11 +0900

    231229-1

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,10 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My Alarm',
+      title: 'My Fear and Greed',
       home: MainPage(),
     );
   }
--- lib/screens/main_page.dart
+++ lib/screens/main_page.dart
@@ -1,12 +1,60 @@
 import 'package:flutter/material.dart';
-//import 'package:flutter/cupertino.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
 
+import '../api/fndapi.dart';
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int fngIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    //fngIndex = getFngIndex() as int;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              child: Text(getFngIndex().toString()),
+            ),
+            /*
+            SizedBox(
+              child: CupertinoButton.filled(
+                onPressed: setState(() {
+                  fngIndex = getFngIndex();
+                }),
+                child: Text('불러오기'), 
+              ),
+            )
+            */
+          ],
+        ),
+      ),
     );
+  }
+
+  Future<int> getFngIndex() async {
+    final url = FnbApi().url;
+    final response = await http.get(Uri.parse(url), headers: FnbApi().headers());
+
+    if(response.statusCode == 200){
+      print('api 응답 : ${response.body}');
+    }
+    else{
+      print('fail : ${response.statusCode}');
+    }
+
+    return 1;
   }
 }
